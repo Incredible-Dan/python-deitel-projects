052ce42715e456042dc91ae9fac8a0461217410c
Incredible_Dan <goldann.c@gmail.com>
2021-02-25 01:43:51 +0100

    Added sum multiples of consecutive integers.

---
+++ sum_of_multiples_of_three_and_five.py
@@ -0,0 +1,13 @@
+
+#Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+
+user_number = int(input('Enter the number: '))
+total = 0
+counter = 0
+
+while user_number > 0:
+    if user_number % 3 == 0 & user_number % 5 == 0:
+        counter = counter + 1
+    total = total + counter
+    user_number = user_number - 1
+print(total)
